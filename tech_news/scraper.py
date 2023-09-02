df48a7220e73d7c3ed838f46590022d76d382b5d
anadolacio <anadolacio@hotmail.com>
2023-09-02 10:49:29 -0300

    resolução requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,7 +38,13 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # raise NotImplementedError
+    try:
+        selector = Selector(text=html_content)
+        result = selector.css(".next::attr(href)").get()
+        return result
+    except Exception:
+        return None
 
 
 # Requisito 4
